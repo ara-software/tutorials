62a16c186f888c18fb30e91b9789aea380dd0aa1
Brian Clark <clark.2668@osu.edu>
2019-05-16 22:08:45 -0400

    add initial analysis directory

---
+++ analysis/build/CMakeFiles/exampleAnalysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/exampleAnalysis.dir/example_ara_analysis.cxx.o"
+  "exampleAnalysis.pdb"
+  "exampleAnalysis"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/exampleAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
