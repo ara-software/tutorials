5c4e2f1c2d5d7eba305e95ae6ff4b82f09eedce4
clark2668 <baclark@msu.edu>
2020-07-14 20:32:03 -0500

    fix mistake in the copy command

--- simulation/generate_arasim/htcondor_version/job.sh
+++ simulation/generate_arasim/htcondor_version/job.sh
@@ -7,4 +7,4 @@ source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
 cd /cvmfs/ara.opensciencegrid.org/trunk/centos7/source/AraSim
 
 ./AraSim $setup_file $index $TMPDIR
-cp $TMPDIR/*'*.root' $output_dir
+cp $TMPDIR/'*.root' $output_dir
